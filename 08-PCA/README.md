2c953287e56b6321f1bc83349fa87bf0f366a9e8
buruzaemon <joker@io>
2012-10-03 22:28:38 +0900

    Fleshing out notes on PCA, etc., in Ch. 8

--- 08-PCA/README.md
+++ 08-PCA/README.md
@@ -1,8 +1,24 @@
 # NOTES: Ch. 8 - PCA: Building a Market Index
 
 ### Tips & Tricks 
-- 
-
+- As you cannot assume data type when reading in data files, you will often have to transform data before you can use it.
+- `lubridate` package's `ymd` parses date and transforms to a formatted string representation in the given timezone (default to UTC).
+- `reshape` package's `cast` will alter a data frame into the specified shape or aggregated form.
+- `reshape` package's `melt` melts an object (data.frame, array, list) into a form that is easy to `cast`.
 
 ### Observations
--
+- Steps for using PCA
+  1. calculate correlations between columns in data matrix
+  2. visually inspect a density plot; iff correlations are mostly positive, then PCA should work for given data set
+  3. calculate principal components matrix with `princomp`
+  4. confirm standard deviations and proportions of variance per component; 1st one should be the one to choose, as the components are already returned in order of decreasing importance (eigenvalue???)
+  5. create a one-column (principal column) summary of data set with `predict(pca)[,1]`
+  6. compare this PCA summary with actual data from a known-quantity, perhaps by plotting both in `ggplot`
+```
+    ggplot(comparison.df, aes(x = Date, y = Price, group = Index, color = Index))+
+      geom_point() +
+      geom_line()
+```
+                             
+
+
--- 08-PCA/chapter08.R
+++ 08-PCA/chapter08.R
@@ -55,6 +55,7 @@ ggplot(data.frame(Correlation = correlations),
 pca <- princomp(date.stock.matrix[,2:ncol(date.stock.matrix)])
 
 # Snippet 7
+pca
 #Call:
 #princomp(x = date.stock.matrix[, 2:ncol(date.stock.matrix)])
 #Standard deviations:
@@ -67,6 +68,39 @@ pca <- princomp(date.stock.matrix[,2:ncol(date.stock.matrix)])
 #Comp.22 Comp.23 Comp.24
 #0.8583681 0.7390626 0.4347983
 #24 variables and 2366 observations.
+
+summary(pca)
+# Importance of components:
+#                            Comp.1     Comp.2      Comp.3      Comp.4     Comp.5
+# Standard deviation     29.1001249 20.4403404 12.67269243 11.46364500 8.49638196
+# Proportion of Variance  0.4600083  0.2269615  0.08723961  0.07138737 0.03921426
+# Cumulative Proportion   0.4600083  0.6869698  0.77420941  0.84559678 0.88481104
+#
+#                            Comp.6     Comp.7     Comp.8     Comp.9     Comp.10
+# Standard deviation     8.19693448 5.54383078 5.13009309 4.77867520 4.257509927
+# Proportion of Variance 0.03649882 0.01669536 0.01429639 0.01240483 0.009846622
+# Cumulative Proportion  0.92130986 0.93800523 0.95230162 0.96470645 0.974553074
+#
+#                           Comp.11     Comp.12     Comp.13     Comp.14
+# Standard deviation     3.305093119 2.619771465 2.498618085 2.174612539
+# Proportion of Variance 0.005933943 0.003728231 0.003391374 0.002568856
+# Cumulative Proportion  0.980487017 0.984215247 0.987606622 0.990175477
+#
+#                            Comp.15     Comp.16     Comp.17     Comp.18
+# Standard deviation     1.946947496 1.870623998 1.698404287 1.634411605
+# Proportion of Variance 0.002059133 0.001900855 0.001566961 0.001451105
+# Cumulative Proportion  0.992234610 0.994135465 0.995702426 0.997153531
+#
+#                            Comp.19      Comp.20      Comp.21      Comp.22
+# Standard deviation     1.232747064 1.1280913254 0.9877633997 0.8583681320
+# Proportion of Variance 0.000825513 0.0006912967 0.0005300072 0.0004002424
+# Cumulative Proportion  0.997979044 0.9986703406 0.9992003478 0.9996005903
+#
+#                             Comp.23      Comp.24
+# Standard deviation     0.7390625523 0.4347982692
+# Proportion of Variance 0.0002967142 0.0001026955
+# Cumulative Proportion  0.9998973045 1.0000000000
+
 
 # Snippet 8
 principal.component <- pca$loadings[, 1]
